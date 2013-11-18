2858460e34b80265a70352d629842119d2a45f16
Alexei Vidmich <alexei@vidmich.com>
2013-11-18 01:27:45 -0500

    adding latest schema to git

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131115082706) do
+ActiveRecord::Schema.define(version: 20131115082522) do
 
   create_table "games", force: true do |t|
     t.string   "event"
